ec4c173b10a8f8507d7f87551073f667d2725d6b
rayv <ray@foxmail.com>
2018-12-24 15:33:48 +0800

    test

---
+++ SpringBootApplication.java
@@ -0,0 +1,7 @@
+package com.broada;
+@SpringbootApplication
+public class SpringBootApplication {
+    public static void main(String[] args) {
+        SpringBootApplication.run(DemoApplication.class, args);
+    }
+}
