538bac9762d2c4e50151f2329f03b556d783dc8c
rayv0703 <44130879+rayv0703@users.noreply.github.com>
2019-03-07 23:06:43 +0800

    123

--- SpringBootApplication.java
+++
@@ -1,7 +0,0 @@
-package com.broada;
-@SpringbootApplication
-public class SpringBootApplication {
-    public static void main(String[] args) {
-        SpringBootApplication.run(DemoApplication.class, args);
-    }
-}
